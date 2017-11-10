7f6475bd937832d05b15f42ce2bde4ad52934d1a
Qiao Liang <qiaoliang@Qiaos-MacBook-Pro.local>
2017-11-09 23:42:42 -0600

    Implemented IoT Power Relay

--- temp_humid_watcher.py
+++ temp_humid_watcher.py
@@ -3,21 +3,51 @@ from datetime import datetime
 from json import load
 from pymongo import MongoClient
 from Adafruit_DHT import read_retry, DHT22
+from RPi import GPIO
 
 
 with open('config.json') as config:
     config_data = load(config)
 
-client = MongoClient()
-db = client.local
+MEASURE_INTERVAL = config_data[u"MEASURE_INTERVAL"]
+SAMPLE_INTERVAL = config_data[u"SAMPLE_INTERVAL"]
+SENSOR_PINS = config_data[u"SENSOR_PINS"]
+SWITCH_PIN = config_data[u"SWITCH_PIN"]
+HUMID_UPPER = config_data[u"HUMID_UPPER"]
+HUMID_LOWER = config_data[u"HUMID_LOWER"]
+SENSOR_COUNT = len(SENSOR_PINS)
 
+# GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(SWITCH_PIN, GPIO.OUT)
+switch_on = False
+sleep_time = SAMPLE_INTERVAL
+
+db = MongoClient().local
 
 while True:
-    data = {"date_time": datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
-    for sensor in config_data[u"sensors"]:
-        humid, temp = read_retry(DHT22, sensor)
-        data[str(sensor)] = {"humid": humid, "temp": temp}
+    data = {"dt": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+    avg_humid = 0
+
+    for pin in SENSOR_PINS:
+        humid, temp = read_retry(DHT22, pin)
+        avg_humid += humid
+        data[str(pin)] = {"h": round(humid, 3), "t": round(temp, 3)}
+    
+    avg_humid /= SENSOR_COUNT
+
+    if switch_on:
+        if avg_humid > HUMID_UPPER:
+           GPIO.output(SWITCH_PIN, GPIO.LOW)
+           sleep_time = SAMPLE_INTERVAL
+           switch_on = False
+           db.switch_log.insert_one({"dt": data["dt"], "on": 0})
+    else:
+        if avg_humid < HUMID_LOWER:
+            GPIO.output(SWITCH_PIN, GPIO.HIGH)
+            sleep_time = MEASURE_INTERVAL
+            switch_on = True
+            db.switch_log.insert_one({"dt": data["dt"], "on": 1})
 
     db.temp_humid.insert_one(data)
-
-    sleep(config_data[u"sleep_sec"])
+    sleep(sleep_time)
